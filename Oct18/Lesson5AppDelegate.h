ed98b382acccaf9cc9c579780296964b20c6e8b5
Seiichi Ito <genius@msj.biglobe.ne.jp>
2012-10-16 15:34:54 -0400

    first commit

---
+++ Oct18/Lesson5AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Lesson5AppDelegate.h
+//  Oct18
+//
+//  Created by MacBook on 10/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Lesson5AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
